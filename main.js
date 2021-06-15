a206025505dfb77a4a29ba006eba95da22535211
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-06-15 11:37:50 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,9 @@ Webcam.set({
             document.getElementById("cobwebcam").innerHTML ='<img id="Halla" src="'+data_uri+'">'
         });
     }
+
+    console.log(ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/iNZXwoBiw/model.json",loaded);
+function loaded() {
+console.log("Did the model really load?");
+}
