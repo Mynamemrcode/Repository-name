32140cbeab7feeccf6f9658c1a714a7a73cb7511
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-06-18 20:04:51 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,16 +4,31 @@ Webcam.set({
     image_format:'png',
     png_quality:90
     })
-    webcam = document.getElementById("webcam");
-    Webcam.attach(webcam);
+    cam = document.getElementById("webcam");
+    Webcam.attach(cam);
     function sni() {
         Webcam.snap(function(data_uri) {
-            document.getElementById("cobwebcam").innerHTML ='<img id="Halla" src="'+data_uri+'">'
+            document.getElementById("cobwebcam").innerHTML ='<img id="hallo" src="'+data_uri+'">'
         });
+        image = document.getElementById("hallo");
+        classifier.classify(image, gottheResult)
     }
+    function gottheResult(errors, result) {
+        if (errors) {
+            console.log(errors);
+        }
+            else {
+                console.log(result);
+                document.getElementById("Object").innerHTML = result[0].label;
+                document.getElementById("Accuracy").innerHTML = result[0].confidence.toFixed(2);
+
+            }
+        }
+    
 
     console.log(ml5.version);
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/iNZXwoBiw/model.json",loaded);
 function loaded() {
 console.log("Did the model really load?");
 }
+
